f0ef83bf4babd6e4461323a0a8283dd217b318ea
Mohit482006 <mohitsinghbha9399@gmail.com>
2024-10-02 06:03:06 +0530

    Add files via upload

---
+++ day 4 THREETOPICS.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes herent 
+	int a,b,c,x;
+	cin>>a>>b>>c>>x;
+	if(a==x||b==x||c==x){
+	    
+	    cout<<"yes"<<endl;
+	}else{
+	    cout<<"no"<<endl;
+	}
+
+}
