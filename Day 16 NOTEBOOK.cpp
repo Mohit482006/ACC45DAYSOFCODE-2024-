0dc00e36d720c12a44f7295089fbdec611f4483b
Mohit482006 <mohitsinghbha9399@gmail.com>
2024-10-14 12:28:31 +0530

    Add files via upload

---
+++ Day 16 NOTEBOOK.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int q=n*1000;
+	    int y=q/100;
+	    cout<<y<<endl;
+	}
+
+}
