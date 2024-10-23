964e8ddf6096bc01734a3d289ec1b7513945743a
Mohit482006 <mohitsinghbha9399@gmail.com>
2024-10-23 10:40:18 +0530

    Add files via upload

---
+++ Day 20 CHEFGAMES.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes hereint t;
+	int t;
+	cin>>t;
+	while(t--){
+	    int r1,r2,r3,r4;
+	    cin>>r1>>r2>>r3>>r4;
+	    if(r1==1||r2==1||r3==1||r4==1){
+	        cout<<"out"<<endl;
+	    }else{
+	        cout<<"IN"<<endl;
+	    }
+	}
+
+}
